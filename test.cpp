df4831e8a416b17bb34d84caa581f6f2592a3001
grrr2828 <grrr_28@163.com>
2020-10-14 16:05:32 +0800

    add scan
    add scan

--- test.cpp
+++ test.cpp
@@ -1,8 +1,44 @@
+#include <fstream>
 #include <iostream>
+#include <vector>
 using namespace std;
+
+
+const int maxLine = 50;
+void scan(const char* fileName, vector<char>& buffer)
+{
+    ifstream in( fileName );
+    if( !in.is_open() )
+    { 
+       return;
+    }
+
+    char lineBuffer[maxLine];
+    
+    while( !in.eof() )
+    {
+        in.getline( lineBuffer, maxLine );
+
+        for (size_t i = 0; i < maxLine; i++)
+        {
+            char ch = lineBuffer[ i ];
+            if( ch == '\000' ){
+                break;
+            }
+
+            buffer.push_back(ch);
+        }
+    }
+
+    in.close();
+}
+
 int main()
 {
-    cout << "Hello World!!!" << endl;
+
+    vector<char> buffer;
+    scan("test.txt", buffer);
+
     system("pause");
     return 0;
 }
